69bce0e2f455bb6fe33f1f74abf527bcbbb2a9c0
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2019-10-24 00:43:52 +0530

    add router and add navbar

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+export default function Navigation() {
+  return (
+    <nav className="nav">
+      <h1 className="nav__title">BeautifulSearch</h1>
+      <div className="nav__links">
+        <Link className="nav__link" to="/">Home</Link>
+        <Link className="nav__link" to="/">Dashboard</Link>
+      </div>
+    </nav>
+  );
+}

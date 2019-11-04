2730d3793faf6ae7407ffdde8f46aeee56b700cf
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2019-11-04 16:35:27 +0530

    make schema page dynamic

--- src/services/solr.js
+++ src/services/solr.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class Solr {
   constructor(core) {
-    this.core = core //|| localStorage.getItem('core');
+    this.core = core || localStorage.getItem('core');
     let url = localStorage.getItem('instance');
     if (!url) {
       throw Error('Cannot create a solr instance without a valid url');
@@ -18,11 +18,12 @@ export default class Solr {
   }
 
   getSchema(core=this.core) {
-    return this.instance.get(`/api/cores/${core}/schema`);
+    // return this.instance.get(`/api/cores/${core}/schema`);
+    return this.instance.get(`/${core}/schema/fields`);
   }
 
   getFields(core=this.core) {
-    return this.instance.get(`/api/cores/${core}/schema/fields`);
+    return this.instance.get(`/${core}/schema/fieldtypes`);
   }
 
   getStatus(core=this.core) {
--- src/views/Schema.js
+++ src/views/Schema.js
@@ -1,42 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Solr from '../services/solr';
 
-const schemaAttributes = ["id",
-  "description",
-  "nps_link",
-  "location",
-  "states",
-  "title",
-  "visitors"
-];
+// schemaAttributes.forEach((val, index) => {
+//   schemaAttributesToReturn.push(
+    // <tr className="schema__attribute" key={index}>
+    //   <td>{val}</td>
+    //   <td>
+    //     <select className="attribute__value">
+    //       <option>{attributeValue[0]}</option>
+    //       <option>{attributeValue[1]}</option>
+    //       <option>{attributeValue[2]}</option>
+    //       <option>{attributeValue[3]}</option>
+    //     </select>
+    //   </td>
+    // </tr>
+// });
 
-const attributeValue = [ "text", "number", "date", "geolocation" ];
-
-let schemaAttributesToReturn = [];
-
-schemaAttributes.forEach((val, index) => {
-  schemaAttributesToReturn.push(
-    <tr className="schema__attribute" key={index}> 
-      <td>{val}</td>
-      <td> 
-        <select className="attribute__value">
-          <option>{attributeValue[0]}</option>
-          <option>{attributeValue[1]}</option>
-          <option>{attributeValue[2]}</option>
-          <option>{attributeValue[3]}</option>
-        </select>
-      </td>
-    </tr>)
-});
 
 export default function Schema() {
+  const [schemaAttributes, setSchemaAttributes] = useState([]);
+  const [fieldTypes, setFieldTypes] = useState([]);
+
+  // schema
+  const fetchSchemaAttributes = async () => {
+    const solr = new Solr();
+    const { data } = await solr.getSchema();
+    setSchemaAttributes(data.fields);
+  }
+
+  useEffect(() => {
+    fetchSchemaAttributes();
+  }, []);
+
+  const filteredSchemaAttributes = () => {
+    return schemaAttributes.filter(a => !a.name.startsWith('_'));
+  }
+
+
+  // field types
+  const fetchFieldTypes = async () => {
+    const solr = new Solr();
+    const { data } = await solr.getFields();
+    setFieldTypes(data.fieldTypes);
+  }
+
+  useEffect(() => {
+    fetchFieldTypes();
+  }, []);
+
+  const filteredFieldTypes = () => {
+    return fieldTypes.filter(ft => !ft.name.startsWith('_'));
+  }
+
+  const onFieldTypeChange = (index, attribute, type) => {
+    console.log(index, attribute, type);
+  }
+
   return (
     <div className="schema">
       <div className="schema__container">
         <div>
           <h1 className="schema__welcome-text">Manage Engine Schema</h1>
-          <div className="schema__headline">Add new fields or change the types of existing ones.</div>
+          <div className="schema__headline">
+            Add new fields or change the types of existing ones.
+          </div>
         </div>
-        <div style={{display: "flex"}}>
+        <div style={{ display: "flex" }}>
           <button className="create-schema__button button--primary">
             Create a Schema Field
           </button>
@@ -48,7 +77,20 @@ export default function Schema() {
       <div className="schema-attribute__container">
         <table className="schema-container__table">
           <tbody>
-            { schemaAttributesToReturn }
+            {filteredSchemaAttributes().map((a, index) => (
+              <tr className="schema__attribute" key={a.name}>
+                <td>{a.name}</td>
+                <td>
+                  <select className="attribute__value" value={a.type} onChange={(e) => onFieldTypeChange(index, a.name, e.target.value)}>
+                    {
+                      filteredFieldTypes().map( ft => (
+                        <option value={ft.name} key={ft.name}>{ft.name}</option>
+                      ))
+                    }
+                  </select>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>

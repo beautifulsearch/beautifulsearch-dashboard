9754d94fb278e82ec4f0c694b399af13022ed81b
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2019-11-06 14:47:09 +0530

    style modal, add markup to add new field

--- src/views/Schema.js
+++ src/views/Schema.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from "react";
+import Modal from 'react-modal';
 import Solr from '../services/solr';
 
 export default function Schema() {
   const [schemaAttributes, setSchemaAttributes] = useState([]);
   const [fieldTypes, setFieldTypes] = useState([]);
+  const [showingAddFieldModal, toggleAddFieldModal ] = useState(false);
 
   // schema
   const fetchSchemaAttributes = async () => {
@@ -62,7 +64,10 @@ export default function Schema() {
           </div>
         </div>
         <div style={{ display: "flex" }}>
-          <button className="create-schema__button button--primary">
+          <button
+            className="create-schema__button button--primary"
+            onClick={() => toggleAddFieldModal(true)}
+          >
             Create a Schema Field
           </button>
           <button className="create-schema__button button--secondary">
@@ -77,12 +82,16 @@ export default function Schema() {
               <tr className="schema__attribute" key={a.name}>
                 <td>{a.name}</td>
                 <td>
-                  <select className="attribute__value" value={a.type} onChange={(e) => onFieldTypeChange(a.name, e.target.value)}>
-                    {
-                      filteredFieldTypes().map( ft => (
-                        <option value={ft.name} key={ft.name}>{ft.name}</option>
-                      ))
-                    }
+                  <select
+                    className="attribute__value"
+                    value={a.type}
+                    onChange={e => onFieldTypeChange(a.name, e.target.value)}
+                  >
+                    {filteredFieldTypes().map(ft => (
+                      <option value={ft.name} key={ft.name}>
+                        {ft.name}
+                      </option>
+                    ))}
                   </select>
                 </td>
               </tr>
@@ -90,6 +99,41 @@ export default function Schema() {
           </tbody>
         </table>
       </div>
+
+      <Modal
+        isOpen={showingAddFieldModal}
+        contentLabel="Label Here"
+        onRequestClose={() => toggleAddFieldModal(false)}
+        className="modal__content"
+        overlayClassName="modal__overlay"
+      >
+        <h3>Add a new field to schema</h3>
+
+        <div className="form__row">
+          <label>Field Name</label>
+          <input />
+        </div>
+
+        <div className="form__row">
+          <label>Field Type</label>
+          <input />
+        </div>
+
+        <div style={{marginTop: 40}}>
+          <button
+            className="button--primary"
+          >
+            Add Field
+          </button>
+          <button
+            className="button--secondary"
+            onClick={() => toggleAddFieldModal(false)}
+            style={{marginLeft: 20}}
+          >
+            Close Modal
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
